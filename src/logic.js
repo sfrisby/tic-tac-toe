c2ad902c4ae34065c22ab7cc36a56c0d0b675512
Spencer Frisby <frisbysg@gmail.com>
2018-12-31 11:15:13 -0700

    added some more functions

--- src/logic.js
+++ src/logic.js
@@ -33,8 +33,8 @@ function get_player_win_serie() {
 function get_computer_win_serie() {
    var count = 0;
    var empty_square = null;
-   for (i=0; i<series.length; i++) {
-      for (j=0; j<series[i].length; j++) {
+   for (var i=0; i<series.length; i++) {
+      for (var j=0; j<series[i].length; j++) {
          if ($(series[i][j]).attr("src") == cir_img) // TODO: more relatabel variable for computer piece.
             count++;
          if ($(series[i][j]).attr("src") == blank_img)
@@ -50,9 +50,34 @@ function get_computer_win_serie() {
    return empty_sqaure;
 }
 
+/* Given 3 square IDs, provide a blank square ID if found.  */
+function get_empty_square(series) {
+   var img_src = "";
+   var used_square = 0;
+   var square = null;
+   for (var i=0; i<series.length; i++) {
+      img_src = $(series).attr("src");
+      if (img_src == blank_img)
+         return square = series[i];
+   }
+   return square; /* null if none found. */
+}
+
+/* Return true if there are no empty squares in the provided series. */
+function all_squares_used_in_series(series) {
+   var img_src = "";
+    for (var i=0; i<series.length; i++) {
+      img_src = $(series[i]).attr("src");
+      if (img_src == blank_img)
+         return false;
+   }
+   return true;
+}
+
+/* Serially looking for empty square. */
 function get_empty_square() {
-   for (i=0; i<series.length; i++) {
-      for (j=0; j<series[i].length; j++) {
+   for (var i=0; i<series.length; i++) {
+      for (var j=0; j<series[i].length; j++) {
          if ($(series[i][j]).attr("src") == blank_img)
             return series[i][j];
       }
